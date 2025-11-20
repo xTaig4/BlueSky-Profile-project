48762390d7928d3a108bdd9bb68941d923ef7bbf
Taig4 <xTaig4@gmail.com>
2025-11-20 22:54:30 +0100

    Add skills list with ratings to Skills component
    Replaces placeholder text with a dynamic list of skills and star ratings. Updates styling and background colors for light/dark modes.

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,7 +1,27 @@
 const Skills = () => {
+  const skills = [
+    { name: "TypeScript", rating: 4 },
+    { name: "C#", rating: 5 },
+    { name: "React", rating: 5 },
+    { name: "ASP.NET", rating: 4 },
+    { name: "Next.js", rating: 4 },
+    { name: "Docker", rating: 3 },
+    { name: "HTML, CSS", rating: 5 },
+    { name: "Tailwind CSS", rating: 5 },
+  ];
   return (
-    <div className="border-6 bg-blue-400 w-200 h-150 rounded-2xl text-2xl pt-5 flex flex-col items-center ">
-      Skills Component
+    <div className="border-6 dark:bg-con-night bg-con-light w-200 h-150 rounded-2xl text-2xl pt-5 flex flex-col items-center ">
+      <ul className="text-text grid grid-cols-2 gap-x-20 gap-y-4 p-4">
+        {skills.map((skill) => (
+          <li key={skill.name} className="text-white text-3xl flex gap-5 ">
+            <span className="w-45">{skill.name}</span>
+            <span>
+              {"★".repeat(skill.rating)}
+              {"☆".repeat(5 - skill.rating)}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
